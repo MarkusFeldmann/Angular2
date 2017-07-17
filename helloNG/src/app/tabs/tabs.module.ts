9c138c46339e6cfb5e3903b9c630e0816dca1099
MarkusFeldmann <mf@mind-it.co>
2017-07-17 08:17:00 +0200

    experimental decorators

--- helloNG/src/app/tabs/tabs.module.ts
+++ helloNG/src/app/tabs/tabs.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TabsComponent, TabComponent} from './tabs.component';
+import { TabsComponent, TabComponent } from './tabs.component';
 
 @NgModule({
   imports: [
@@ -10,3 +10,24 @@ import {TabsComponent, TabComponent} from './tabs.component';
   exports: [TabComponent, TabsComponent]
 })
 export class TabsModule { }
+
+interface Monument {
+  name: string;
+  height: number;
+}
+
+class bla {
+  calc() {
+    var m: Monument[] = [];
+    m.push({ name: 'ABC', height: 999.0});
+    m.push({ name: 'DEF', height: 99.0});
+    var orderedMs = m.sort(this.cmpMonument);
+  }
+
+  cmpMonument(m1: Monument, m2: Monument)
+  {
+    if(m1.height > m2.height) return -1;
+    if(m1.height < m2.height) return 1;
+    return 0;
+  }
+}
